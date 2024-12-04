c9a54985313567b309046119092305a650392c8c
Conradv2 <marek226@interia.pl>
2024-12-04 17:58:50 +0100

    final version of ft_isalnum.c

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,31 +6,17 @@
 /*   By: conradv2 <conradv2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 19:30:43 by conradv2          #+#    #+#             */
-/*   Updated: 2024/12/03 19:55:05 by conradv2         ###   ########.fr       */
+/*   Updated: 2024/12/04 17:58:21 by conradv2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <ctype.h>
-
-
-int	ft_isalnum (int c)
+int	ft_isalnum(int c)
 {
-	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9'))
-	{
+	if (c >= 'A' && c <= 'Z')
 		return (1);
-	}
-	return (0);
-}
-
-int	main(void)
-{
-	char c = '!';
-	int result = isalnum(c);
-
-	printf("1 - > character is alphanumeric \n");
-	printf("0 - > character is not alphanumeric\n");
-	printf("result is: %d\n", ft_isalnum(c));
-	printf("ctype.h - > %d\n", result);
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	if (c >= '0' && c <= '9')
+		return (1);
 	return (0);
 }
