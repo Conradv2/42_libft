122984a4f57cfbe08d70d190fc7946104ecafb06
Conradv2 <marek226@interia.pl>
2024-12-04 18:00:03 +0100

    final version of ft_isalpha.c

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,11 +6,9 @@
 /*   By: conradv2 <conradv2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 21:13:29 by conradv2          #+#    #+#             */
-/*   Updated: 2024/12/02 21:26:38 by conradv2         ###   ########.fr       */
+/*   Updated: 2024/12/04 17:59:44 by conradv2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
 
 int	ft_isalpha(int c)
 {
@@ -18,14 +16,5 @@ int	ft_isalpha(int c)
 	{
 		return (c);
 	}
-	return (0);
-}
-
-int	main(void)
-{
-	char	c;
-
-	c = 'z';
-	printf("Character: %d\n", ft_isalpha(c));
 	return (0);
 }
