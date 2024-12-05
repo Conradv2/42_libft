b179237090d50a7099b2d3c7648783f31e83dd14
Conradv2 <marek226@interia.pl>
2024-12-05 22:50:54 +0100

    first try of ft_memset.c

---
+++ ft_memset.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: conradv2 <conradv2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/05 20:34:32 by conradv2          #+#    #+#             */
+/*   Updated: 2024/12/05 22:49:14 by conradv2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include <stdio.h>
+#include <string.h>
+typedef unsigned long int size_t;
+
+
+void	*ft_memset (void*str, unsigned char c, size_t n)
+{
+	unsigned char *ptr = (unsigned char *)str;
+	while (n != 0)
+	{
+		*ptr = c;
+		ptr++;
+		n--;
+	}
+	
+	return str;
+}
+
+
+void printArray(int arr[], int n)
+{
+   for (int i=0; i<n; i++)
+      printf("%d ", arr[i]);
+}
+void char_printArray(char arr[], int n)
+{
+   for (int i=0; i<n; i++)
+      printf("%c ", arr[i]);
+}
+
+int main()
+{
+    int arr[] = {10,9,8,7,6,5,4,3,2,1};
+	char string_array[] = {'a','b','c'};
+    // Fill whole array with 0.
+    memset(&arr[5], 0, 5*sizeof(arr[0]));
+    printf("Array after memset()\n");
+    printArray(arr, 10);
+	printf("\n");
+	int my_arr[] = {1,2,3,4,5,6,7,8,9,10};
+
+	ft_memset(my_arr, 0, 10*sizeof(arr[0]));
+	printf("\nMy array after ft_memset()\n");
+	printArray(my_arr, 10);
+	printf("\n");
+
+	printf("String array: \n");
+	char_printArray(string_array, 3);
+	printf("\n");
+	//ft_memset(string_array, 'Z', 3*sizeof(string_array[0]));
+	ft_memset(string_array, 'Z', 3*sizeof(string_array[0]));
+	char_printArray(string_array, 3);
+	// int c = 0x0002;
+	// printf("\n%d\n", c);
+    return 0;
+}
