a4b3b5e07d25030d9ca98a7ff811ac1a201dfc9c
Konrad Kruszynski <kkruszyn@c1r13s2.42warsaw.pl>
2024-12-07 15:58:58 +0100

    changed a bit ft_strlcpy.c

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,12 +6,11 @@
 /*   By: kkruszyn <kkruszyn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/07 14:27:45 by kkruszyn          #+#    #+#             */
-/*   Updated: 2024/12/07 15:51:41 by kkruszyn         ###   ########.fr       */
+/*   Updated: 2024/12/07 15:58:37 by kkruszyn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
 int	ft_strlen(const char *str)
 {
@@ -26,34 +25,23 @@ int	ft_strlen(const char *str)
 	return (counter);
 }
 
-size_t ft_strlcpy(char *dest, const char *src, size_t size)
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
-	int 	len;
+	int		len;
 	size_t	i;
 
 	len = ft_strlen(src);
 	i = 0;
 	if (size > 0)
 	{
-		while((*src != '\0') && (i < size - 1))
+		while ((*src != '\0') && (i < size - 1))
 		{
 			*dest++ = *src++;
 			i++;
-		}	
+		}
 		*dest = '\0';
 	}
 	else
 		dest[0] = '\0';
-	
 	return (len);
-}
-
-int	main()
-{
-	char dest[50];
-	char source[] = "Hello, world!";
-	int n = ft_strlcpy(dest, source, 13);
-	printf("Dest: %s\n", dest);
-	printf("Length of src: %d\n", n);
-	return (0);
 }
