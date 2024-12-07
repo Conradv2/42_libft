369e631b5bed11f68f634635e43c5ddbce6f8ec5
Konrad Kruszynski <kkruszyn@c1r13s2.42warsaw.pl>
2024-12-07 13:39:44 +0100

    added properly working ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,61 +6,48 @@
 /*   By: kkruszyn <kkruszyn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/07 12:12:14 by kkruszyn          #+#    #+#             */
-/*   Updated: 2024/12/07 13:22:40 by kkruszyn         ###   ########.fr       */
+/*   Updated: 2024/12/07 13:38:57 by kkruszyn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 
-void	*ft_memmove(void*destination, const void*source, size_t num)
-{	
+void	*ft_memcpy(void*destination, const void*source, size_t num)
+{
 	unsigned char	*_dest;
-	unsigned char	*_source; 
-	size_t			i;
+	unsigned char	*_source;
 
-	i = 0;
 	_dest = (unsigned char *)destination;
 	_source = (unsigned char *)source;
-
-	if ((_dest == _source) || num == 0)
-		return (destination);
-
-	if ((_dest < _source) || (_dest >= _source + num))
+	while (num != 0)
 	{
-		while (i < num)
-		{
-			*_dest = *_source;
-			_dest++;
-			_source++;
-			i++;
-		}
-		i = num;
+		*_dest++ = *_source++;
+		num--;
 	}
-	else 
-		while (i > 0)
-		{
-			_dest[i - 1] = _source[i - 1];
-			_dest++;
-			_source++;
-			i--;
-		}
 	return (destination);
 }
 
-int main() 
-{ 
-	char	csrc[100] = "SIEMANO KURWA";
-	char	CHUJ[100] = " WSZYSTKIM";
+void	*ft_memmove(void*destination, const void*source, size_t num)
+{
+	unsigned char	*_dest;
+	unsigned char	*_source;
+	size_t			i;
 
-	char	myTAB_dest[100] = "SIEMANO KURWA";
-	char	myTAB_source[100] = " WSZYSTKIM";
-	
-	memmove(csrc + 13, CHUJ, sizeof(CHUJ) - 13);
-	printf("%s\n", csrc); 
-
-	ft_memmove(myTAB_dest + 13, myTAB_source, sizeof(myTAB_source) - 13);
-	printf("%s\n", myTAB_dest);
-	return 0; 
+	_dest = (unsigned char *)destination;
+	_source = (unsigned char *)source;
+	if ((_dest == _source) || num == 0)
+		return (destination);
+	if ((_dest < _source) || (_dest >= _source + num))
+	{
+		_dest = ft_memcpy(_dest, _source, num);
+	}
+	else
+	{
+		while (num > 0)
+		{
+			_dest[num - 1] = _source[num - 1];
+			num--;
+		}
+	}
+	return (destination);
 }
