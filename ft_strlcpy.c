c1564f4cc924abb771f4468a5a8a1df93c7dce49
Konrad Kruszynski <kkruszyn@c1r12s4.42warsaw.pl>
2024-12-13 16:11:36 +0100

    removed additional functions in ft_strlcat and ft_strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -3,27 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: conradv2 <conradv2@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kkruszyn <kkruszyn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/07 14:27:45 by kkruszyn          #+#    #+#             */
-/*   Updated: 2024/12/09 02:29:52 by conradv2         ###   ########.fr       */
+/*   Updated: 2024/12/13 16:06:55 by kkruszyn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-int	ft_strlen(const char *str)
-{
-	int	counter;
-
-	counter = 0;
-	while (*str != '\0')
-	{
-		counter++;
-		str++;
-	}
-	return (counter);
-}
 
 size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
