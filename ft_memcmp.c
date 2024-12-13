6f7f69c4a6eb81162cc98350e2a0ce27474be238
Konrad Kruszynski <kkruszyn@c1r12s4.42warsaw.pl>
2024-12-13 18:54:15 +0100

    changed while condition so the function won't run past n bytes

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: kkruszyn <kkruszyn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 01:38:05 by conradv2          #+#    #+#             */
-/*   Updated: 2024/12/13 16:29:03 by kkruszyn         ###   ########.fr       */
+/*   Updated: 2024/12/13 18:53:16 by kkruszyn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,12 @@ int	ft_memcmp(const void*str1, const void*str2, size_t n)
 	str1_ptr = (unsigned char *)str1;
 	str2_ptr = (unsigned char *)str2;
 	i = 0;
-	while ((str1_ptr[i] == str2_ptr[i]) && str1_ptr[i] && str2_ptr[i] && n > 0)
+	while (n > 0)
 	{
-		if (str1_ptr[i] == '\0')
-			return (0);
+		if (str1_ptr[i] != str2_ptr[i])
+			return (str1_ptr[i] - str2_ptr[i]);
 		i++;
 		n--;
 	}
-	if (n == 0)
-		return (0);
-	return ((unsigned char)str1_ptr[i] - (unsigned char)str2_ptr[i]);
+	return (0);
 }
