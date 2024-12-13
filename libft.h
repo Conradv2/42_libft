6e770ff822ea56ba6cf04630bb780358c4afaa06
Konrad Kruszynski <kkruszyn@c1r12s4.42warsaw.pl>
2024-12-13 13:59:13 +0100

    updated libft.h

--- libft.h
+++ libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: conradv2 <conradv2@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kkruszyn <kkruszyn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 02:02:36 by conradv2          #+#    #+#             */
-/*   Updated: 2024/12/09 02:26:59 by conradv2         ###   ########.fr       */
+/*   Updated: 2024/12/13 13:58:54 by kkruszyn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ int				ft_isalpha(int c);
 int				ft_isascii(int c);
 int				ft_isdigit(int c);
 int				ft_isprint(int c);
+void	        *ft_memchr(const void*ptr, int ch, size_t count);
+int	            ft_memcmp(const void*str1, const void*str2, size_t n);
 void			*ft_memcpy(void*destination, const void*source, size_t num);
 void			*ft_memmove(void*destination, const void*source, size_t num);
 void			*ft_memset(void*str, int c, size_t n);
@@ -26,6 +28,8 @@ char			*ft_strchr(const char *str, int search_str);
 size_t			ft_strlcat(char *dest, const char *src, size_t size);
 size_t			ft_strlcpy(char *dest, const char *src, size_t size);
 unsigned int	ft_strlen(char*str);
+int         	ft_strncmp(const char*str1, const char*str2, size_t n);
+char	        *strnstr(const char *big, const char *little, size_t len);
 char			*ft_strrchr(const char *str, int search_str);
 int				ft_tolower(int c);
 int				ft_toupper(int c);
