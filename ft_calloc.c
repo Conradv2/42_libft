fb2a9d58c8f2bead20457a481057075227657da5
Conradv2 <marek226@interia.pl>
2024-12-15 18:39:33 +0100

    iteration from nitems to nitems * size

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: conradv2 <conradv2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/14 21:35:42 by conradv2          #+#    #+#             */
-/*   Updated: 2024/12/15 18:29:45 by conradv2         ###   ########.fr       */
+/*   Updated: 2024/12/15 18:39:08 by conradv2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	*ft_calloc(size_t nitems, size_t size)
 	i = 0;
 	if(str == NULL)
 		return (NULL);
-	while (i < nitems)
+	while (i < nitems * size)
 	{
 		str[i] = 0;
 		i++;
