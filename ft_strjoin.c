9f189a0f3283ae3f1a334c1ae79da63971ec1630
Konrad Kruszynski <kkruszyn@c3r3s3.42warsaw.pl>
2024-12-20 16:54:42 +0100

    ft_strjoin.c working properly

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: kkruszyn <kkruszyn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 14:49:31 by kkruszyn          #+#    #+#             */
-/*   Updated: 2024/12/20 15:28:05 by kkruszyn         ###   ########.fr       */
+/*   Updated: 2024/12/20 16:54:24 by kkruszyn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,20 +23,20 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	i = 0;
 	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
+	if (!s1 || !s2)
+		return (NULL);
 	s3 = (char *)malloc((s1_len + s2_len) + 1 * sizeof(char));
 	if (s3 == NULL)
 		return (NULL);
-	while (s1[i] != '\0')
+	while (i < s1_len)
 	{
 		s3[i] = s1[i];
 		i++;
 	}
 	tmp = i;
 	i = 0;
-	while (s3[i++] != '\0')
-	{
-		s3[tmp++] = s2[i];
-	}
-	s3[i] = '\0';
+	while (i < s2_len)
+		s3[tmp++] = s2[i++];
+	s3[tmp] = '\0';
 	return (s3);
 }
