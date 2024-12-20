d180bb5bfd3f506f027e05c396086214bf60b683
Konrad Kruszynski <kkruszyn@c3r3s3.42warsaw.pl>
2024-12-20 17:43:59 +0100

    ft_strtrim func declaration

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: kkruszyn <kkruszyn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 02:02:36 by conradv2          #+#    #+#             */
-/*   Updated: 2024/12/20 15:29:38 by kkruszyn         ###   ########.fr       */
+/*   Updated: 2024/12/20 17:41:44 by kkruszyn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,13 +33,14 @@ void		*ft_memmove(void*destination, const void*source, size_t num);
 void		*ft_memset(void*str, int c, size_t n);
 char		*ft_strchr(const char *str, int search_str);
 char		*ft_strdup( const char *str1 );
-char	    *ft_strjoin(char const *s1, char const *s2);
+char		*ft_strjoin(char const *s1, char const *s2);
 size_t		ft_strlcat(char *dest, const char *src, size_t size);
 size_t		ft_strlcpy(char *dest, const char *src, size_t size);
 size_t		ft_strlen(const char*str);
 int			ft_strncmp(const char*str1, const char*str2, size_t n);
 char		*ft_strnstr(const char *big, const char *little, size_t len);
 char		*ft_strrchr(const char *str, int search_str);
+char		*ft_strtrim(char const *s1, char const *set);
 char		*ft_substr(char const *s, unsigned int start, size_t len);
 int			ft_tolower(int c);
 int			ft_toupper(int c);
