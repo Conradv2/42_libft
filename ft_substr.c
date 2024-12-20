bf8074443fce58f04f08c095521e0bf51ac28ea6
Konrad Kruszynski <kkruszyn@c1r12s4.42warsaw.pl>
2024-12-20 13:21:26 +0100

    ft_substr.c v.1.2

--- ft_substr.c
+++ ft_substr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: conradv2 <conradv2@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kkruszyn <kkruszyn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 20:01:00 by conradv2          #+#    #+#             */
-/*   Updated: 2024/12/19 22:11:52 by conradv2         ###   ########.fr       */
+/*   Updated: 2024/12/20 13:20:59 by kkruszyn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,20 +22,18 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		return (NULL);
 	if (start >= ft_strlen(s))
 	{
-		_substr = (char *)malloc(1 * sizeof(char));
-		if (_substr == NULL)
-			return (NULL);
-		_substr[0] = '\0';
+		_substr = ft_strdup("");
 		return (_substr);
 	}
-	_substr = (char *)malloc((len + 1) * sizeof(char));
+	if (len >= ft_strlen(s))
+		_substr = (char *)malloc((ft_strlen(s) + 1) * sizeof(char));
+	if (len < ft_strlen(s))
+		_substr = (char *)malloc((len + 1) * sizeof(char));
 	if (_substr == NULL)
 		return (NULL);
-	while (i < len && s[start] != '\0')
+	while ((i < len && i < ft_strlen(s)) && s[start] != '\0')
 	{
-		_substr[i] = s[start];
-		i++;
-		start++;
+		_substr[i++] = s[start++];
 	}
 	_substr[i] = '\0';
 	return (_substr);
