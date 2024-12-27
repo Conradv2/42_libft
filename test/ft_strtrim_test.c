b9935354f63ce8fe406e0e2afd2c02dc4ac43640
Conradv2 <marek226@interia.pl>
2024-12-27 17:25:30 +0100

    test main

--- test/ft_strtrim_test.c
+++ test/ft_strtrim_test.c
@@ -1,11 +1,13 @@
 int	main(void)
 {
-	char s1[] = " TEST ";
-	char s2[] = " ";
+	char s1[] = "            321TESTss      ";
+	char s2[] = " ";	
+	char *s3 = ft_strtrim(s1,s2);
 
-	printf("%s\n", s1);
-	printf("%s\n", s2);
-	printf("%s\n", ft_strtrim(s1,s2));
-	printf("%s\n", s1);
+	printf("s1 = %s\n", s1);
+	printf("s2 = %s\n", s2);
+	printf("cp = %s\n", s3);
+	printf("s1 = %s\n", s1);
+	free(s3);
 	return (0);
 }
