fabab9387107f5be9c61ee63ef7efe5fe8e9e4d9
Conradv2 <marek226@interia.pl>
2025-01-02 14:45:14 +0100

    added ft_strmapi and main

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: conradv2 <conradv2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 02:02:36 by conradv2          #+#    #+#             */
-/*   Updated: 2025/01/01 13:59:47 by conradv2         ###   ########.fr       */
+/*   Updated: 2025/01/02 13:25:51 by conradv2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,19 +26,20 @@ int			ft_isalpha(int c);
 int			ft_isascii(int c);
 int			ft_isdigit(int c);
 int			ft_isprint(int c);
-char	    *ft_itoa(int n);
+char		*ft_itoa(int n);
 void		*ft_memchr(const void*ptr, int ch, size_t count);
 int			ft_memcmp(const void*str1, const void*str2, size_t n);
 void		*ft_memcpy(void*destination, const void*source, size_t num);
 void		*ft_memmove(void*destination, const void*source, size_t num);
 void		*ft_memset(void*str, int c, size_t n);
-char    	**ft_split(char const *s, char c);
+char		**ft_split(char const *s, char c);
 char		*ft_strchr(const char *str, int search_str);
 char		*ft_strdup( const char *str1 );
 char		*ft_strjoin(char const *s1, char const *s2);
 size_t		ft_strlcat(char *dest, const char *src, size_t size);
 size_t		ft_strlcpy(char *dest, const char *src, size_t size);
 size_t		ft_strlen(const char*str);
+char		*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 int			ft_strncmp(const char*str1, const char*str2, size_t n);
 char		*ft_strnstr(const char *big, const char *little, size_t len);
 char		*ft_strrchr(const char *str, int search_str);
