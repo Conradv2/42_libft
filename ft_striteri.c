2a69f48ea9b0871da4856435e3f59523ff8eacb7
Conradv2 <marek226@interia.pl>
2025-01-06 12:48:44 +0100

    changed i and len to unsigned int

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: conradv2 <conradv2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 15:11:41 by conradv2          #+#    #+#             */
-/*   Updated: 2025/01/02 16:13:05 by conradv2         ###   ########.fr       */
+/*   Updated: 2025/01/06 12:45:30 by conradv2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
-	int	i;
-	int	len;
+	unsigned int	i;
+	unsigned int	len;
 
 	len = ft_strlen(s);
 	i = 0;
