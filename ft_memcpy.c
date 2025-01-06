e9ffbcbd36375bf46d0697ea7edbf3c403c3bde1
Conradv2 <marek226@interia.pl>
2025-01-06 12:33:34 +0100

    changed _source to const qualifier

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: conradv2 <conradv2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/07 12:12:14 by kkruszyn          #+#    #+#             */
-/*   Updated: 2024/12/09 02:29:12 by conradv2         ###   ########.fr       */
+/*   Updated: 2025/01/06 12:32:36 by conradv2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 void	*ft_memcpy(void*destination, const void*source, size_t num)
 {
-	unsigned char	*_dest;
-	unsigned char	*_source;
+	unsigned char		*_dest;
+	const unsigned char	*_source;
 
 	_dest = (unsigned char *)destination;
-	_source = (unsigned char *)source;
+	_source = (const unsigned char *)source;
 	while (num != 0)
 	{
 		*_dest++ = *_source++;
