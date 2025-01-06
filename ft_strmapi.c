d6c7df6625c62244b9d5bdfe4458acfc2b0a73c4
Conradv2 <marek226@interia.pl>
2025-01-06 13:05:29 +0100

    s == null checked at the start of a function

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: conradv2 <conradv2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 13:25:53 by conradv2          #+#    #+#             */
-/*   Updated: 2025/01/02 14:41:21 by conradv2         ###   ########.fr       */
+/*   Updated: 2025/01/06 13:04:27 by conradv2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,16 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
 	char	*s_copy;
 	size_t	i;
+	size_t	s_len;
 
-	i = 0;
 	if (s == NULL)
 		return (NULL);
-	s_copy = (char *)malloc((ft_strlen(s) + 1) * sizeof(char));
+	i = 0;
+	s_len = ft_strlen(s);
+	s_copy = (char *)malloc((s_len + 1) * sizeof(char));
 	if (s_copy == NULL)
 		return (NULL);
-	while (i < ft_strlen(s))
+	while (i < s_len)
 	{
 		s_copy[i] = f(i, s[i]);
 		i++;
