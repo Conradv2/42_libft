f52159deba7c7655d47293d817717caa73876a4a
Conradv2 <marek226@interia.pl>
2025-01-06 21:14:22 +0100

    added check for null parameters

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: conradv2 <conradv2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/07 12:12:14 by kkruszyn          #+#    #+#             */
-/*   Updated: 2025/01/06 12:32:36 by conradv2         ###   ########.fr       */
+/*   Updated: 2025/01/06 21:11:35 by conradv2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	*ft_memcpy(void*destination, const void*source, size_t num)
 	unsigned char		*_dest;
 	const unsigned char	*_source;
 
+	if (destination == NULL || source == NULL)
+		return (destination);
 	_dest = (unsigned char *)destination;
 	_source = (const unsigned char *)source;
 	while (num != 0)
