76954a2739e436d312bddd9d851d5923e71acc14
Conradv2 <marek226@interia.pl>
2025-01-06 15:43:06 +0100

    changed some logic of str1 and str2 = '\0

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: conradv2 <conradv2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 01:38:05 by conradv2          #+#    #+#             */
-/*   Updated: 2024/12/10 01:59:37 by conradv2         ###   ########.fr       */
+/*   Updated: 2025/01/06 15:39:12 by conradv2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,13 @@ int	ft_strncmp(const char*str1, const char*str2, size_t n)
 {
 	int	i;
 
+	if (n == 0)
+		return (0);
 	i = 0;
-	while ((str1[i] == str2[i]) && str1[i] && str2[i] && n > 0)
+	while ((str1[i] == str2[i]) && n > 0)
 	{
-		if (str1[i] == '\0')
-			return (0);
+		if (str1[i] == '\0' || str2[i] == '\0')
+			break ;
 		i++;
 		n--;
 	}
