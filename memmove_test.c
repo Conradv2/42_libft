31c85a82fe434fd437c8e59eed9dfeeb53eacc69
Conradv2 <marek226@interia.pl>
2025-01-06 19:03:40 +0100

    very good explained memmove_test.c

--- memmove_test.c
+++ memmove_test.c
@@ -6,7 +6,7 @@
 /*   By: conradv2 <conradv2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 16:03:04 by conradv2          #+#    #+#             */
-/*   Updated: 2025/01/06 17:27:55 by conradv2         ###   ########.fr       */
+/*   Updated: 2025/01/06 19:03:18 by conradv2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,13 @@
 
 int main() {
   
-    char source[] = "hello world";
-    printf("%s\n", source);
-    memcpy(source + 2, source, 5);
-    printf("%s\n", source);
-    
-    char source2[] = "hello world";
-    printf("%s\n", source2);
-    ft_memcpy(source2 + 2, source2, 5);
-    printf("%s\n", source2);
+    //                1,2,1,2,3,4,5,6,7,8
+    int source[10] = {1,2,3,4,5,6,7,8,9,10};
+    //                1,2,3,4,5,6,7,8,9,10
+    ft_memmove(source + 2, source, sizeof(int) * 8);
+    for (int i = 0; i < 10; i++)
+    {
+        printf("source[%d] = %d\n", i, source[i]);
+    }
     return 0;
 }
