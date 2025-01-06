a917040e77e04d1ed9c9f282dd375944c05068e0
Conradv2 <marek226@interia.pl>
2025-01-06 12:56:19 +0100

    checks at the start of a func if s1 and s2 are null to prevent further operations

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kkruszyn <kkruszyn@student.42.fr>          +#+  +:+       +#+        */
+/*   By: conradv2 <conradv2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 14:49:31 by kkruszyn          #+#    #+#             */
-/*   Updated: 2024/12/20 16:54:24 by kkruszyn         ###   ########.fr       */
+/*   Updated: 2025/01/06 12:55:23 by conradv2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,11 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	int		i;
 
 	i = 0;
-	s1_len = ft_strlen(s1);
-	s2_len = ft_strlen(s2);
 	if (!s1 || !s2)
 		return (NULL);
-	s3 = (char *)malloc((s1_len + s2_len) + 1 * sizeof(char));
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	s3 = (char *)malloc((s1_len + s2_len + 1) * sizeof(char));
 	if (s3 == NULL)
 		return (NULL);
 	while (i < s1_len)
