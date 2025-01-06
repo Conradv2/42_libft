bd20f0a01fc52c5178347ad66c6204be1b4b4d0b
Conradv2 <marek226@interia.pl>
2025-01-06 19:11:50 +0100

    some minor changes

--- memmove_test.c
+++ memmove_test.c
@@ -6,7 +6,7 @@
 /*   By: conradv2 <conradv2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 16:03:04 by conradv2          #+#    #+#             */
-/*   Updated: 2025/01/06 19:03:18 by conradv2         ###   ########.fr       */
+/*   Updated: 2025/01/06 19:11:32 by conradv2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,8 @@ int main() {
     //                1,2,1,2,3,4,5,6,7,8
     int source[10] = {1,2,3,4,5,6,7,8,9,10};
     //                1,2,3,4,5,6,7,8,9,10
-    ft_memmove(source + 2, source, sizeof(int) * 8);
+    //                1 2 3 1 2 3 4 5 6 7    
+    ft_memmove(source + 3, source, sizeof(int) * 7);
     for (int i = 0; i < 10; i++)
     {
         printf("source[%d] = %d\n", i, source[i]);
